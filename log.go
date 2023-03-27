1424f3ee06f80984bfaec82c048d322ea0540d7b
smart <55507599@qq.com>
2023-03-28 01:58:03 +0800

    add log

---
+++ log.go
@@ -0,0 +1,39 @@
+package speed
+
+import (
+	"io"
+	"log"
+	"os"
+	"runtime"
+)
+
+var errlog *log.Logger
+var infolog *log.Logger
+
+func init() {
+	if _, err := os.Stat("./store"); os.IsNotExist(err) {
+		os.Mkdir("./storage", 0777)
+	}
+	file, err := os.OpenFile("./storage/debug.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln("自定义log包--无法打开文件", err)
+	}
+	errlog = log.New(io.MultiWriter(file, os.Stderr), "error: ", log.Ldate|log.Ltime)
+	infolog = log.New(io.MultiWriter(file, os.Stderr), " info: ", log.Ldate|log.Ltime)
+}
+func LogErr(err ...interface{}) {
+	writeData(errlog, err...)
+}
+func LogInfo(data ...interface{}) {
+	writeData(infolog, data...)
+}
+func writeData(logger *log.Logger, data ...interface{}) {
+	projectPath, _ := os.Getwd()
+	_, file, line, ok := runtime.Caller(2)
+	if data[0] != nil && ok {
+		var slice []interface{}
+		slice = append(slice, file[len(projectPath)+1:], line)
+		slice = append(slice, data...)
+		logger.Println(slice...)
+	}
+}
